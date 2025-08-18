3fc244909b1e110c688ac40b654a82736366dcd5
Roberta Nascimento <60294500+RNask@users.noreply.github.com>
2025-08-18 16:13:53 -0300

    feat: exercicios

--- Bytebank/bytebank_ATENDIMENTO/Program.cs
+++ Bytebank/bytebank_ATENDIMENTO/Program.cs
@@ -1,6 +1,57 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
-int idade = 30;
-//int idade = 20;
-//int idade = 50;
-//int idade = 90;
+void TestaArrayInt()
+{
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 20;
+    idades[2] = 50;
+    idades[3] = 90;
+    idades[4] = 10;
+
+    Console.WriteLine($"Tamanho do Array: {idades.Length}");
+
+    int acumulador = 0;
+
+    for ( int i = 0; i < idades.Length; i++ )
+    {
+        int idade = idades[i];
+        Console.WriteLine($"Idade: {idade}");
+        acumulador += idade;
+    }
+
+    int media = acumulador/idades.Length;
+    Console.WriteLine($"Média de idades = {media}");
+}
+
+void TestaBuscaPalavra()
+{
+    string[] arrayPalavras = new string[5];
+
+    for (int i = 0; i < arrayPalavras.Length; i++)
+    {
+        Console.Write($"Digite {i + 1}ª Palavra: ");
+        arrayPalavras[i] = Console.ReadLine();
+    }
+
+    Console.Write("Digite a palavra a ser encontrada: ");
+    
+    var busca = Console.ReadLine();
+
+    foreach (string palavra in arrayPalavras)
+    {
+        if (palavra.Equals(busca))
+        {
+            Console.WriteLine($"Palavra encontrada = {busca}.");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Palavra não encontrada");
+            break;
+        }
+    }
+}
+
+//TestaArrayInt();
+TestaBuscaPalavra();
