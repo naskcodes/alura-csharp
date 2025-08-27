e2b71719abdc824efb56b7ae9742eead2a15e51b
Roberta Nascimento <60294500+RNask@users.noreply.github.com>
2025-08-27 10:52:06 -0300

    feat: criacao do metodo de busca de palavras e testagem da mediana

--- Bytebank/bytebank_ATENDIMENTO/Program.cs
+++ Bytebank/bytebank_ATENDIMENTO/Program.cs
@@ -45,13 +45,39 @@ void TestaBuscaPalavra()
             Console.WriteLine($"Palavra encontrada = {busca}.");
             break;
         }
-        else
-        {
-            Console.WriteLine("Palavra não encontrada");
-            break;
-        }
     }
 }
 
-//TestaArrayInt();
-TestaBuscaPalavra();
+Array amostra = Array.CreateInstance(typeof(double), 5);
+amostra.SetValue(5.9, 0);
+amostra.SetValue(1.8, 1);
+amostra.SetValue(7.1, 2);
+amostra.SetValue(10, 3);
+amostra.SetValue(6.9, 4);
+
+void TestaMediana(Array array)
+{
+    if ((array == null) || (array.Length == 0))
+    {
+        Console.WriteLine("Array para cálculo de mediana está vazio ou nulo.");
+    }
+
+    double[] numerosOrdenados = (double[]) array.Clone();
+
+    Array.Sort(numerosOrdenados);
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho/2;
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] : numerosOrdenados[meio] + numerosOrdenados[meio - 1] / 2;
+
+    Console.WriteLine($"Com base na amostra a mediana é igual {mediana}");
+}
+
+TestaMediana(amostra);
+
+int[] valores = {10, 58, 36, 47};
+
+for int i = 0;  i < valores.Length; i++)
+{
+
+}
